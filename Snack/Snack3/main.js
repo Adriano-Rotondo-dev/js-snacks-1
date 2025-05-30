db2b5e3dbee3c423d003961e7022461b73bc7579
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 12:12:26 +0200

    Add array to store user inputs and log the numbers entered

--- Snack/Snack3/main.js
+++ Snack/Snack3/main.js
@@ -1,9 +1,12 @@
 // TODO: il software deve chiedere per 10 volte all'ultente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
 
 let somma = 0;
+let numeri = []
 for (let i = 0; i < 10; i++) {
   let numero = Number(prompt("Inserisci un numero intero"));
   somma += numero;
+  numeri.push(numero)
 }
+console.log(numeri)
 console.log(somma);
 alert("La somma dei numeri ottenuti è " + somma);
