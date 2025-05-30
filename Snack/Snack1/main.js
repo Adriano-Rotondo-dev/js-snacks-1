f3d2fe704281ae36b9acc90ed4d743fc42f48603
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 11:14:39 +0200

    Refactor code to prompt user for two numbers and display the larger one

--- Snack/Snack1/main.js
+++ Snack/Snack1/main.js
@@ -1,8 +1,24 @@
 // TODO: l'utente stampa due numeri in successione, con due prompt. Il software stampa il maggiore.
 
-const firstNumber = 10;
+ const firstNumber = 10;
 const secondNumber = 15;
 console.log( firstNumber + " and " + secondNumber);
 
 const biggerNumber = Math.max(firstNumber, secondNumber)
 console.log(biggerNumber);
+
+
+/* come doveva essere nel file perduto, non contarlo 😢
+
+let firstNumber = Number(prompt ("Enter your first choice of number"))
+console.log(firstNumber)
+
+let secondNumber = Number(prompt ("Enter your second choice of number"))
+console.log(secondNumber)
+
+alert("These are your chosen numbers " + firstNumber + " and " + secondNumber )
+
+let largeNumber = Math.max(firstNumber, secondNumber)
+console.log(largeNumber)
+alert("The largest number is " + largeNumber)
+*/
