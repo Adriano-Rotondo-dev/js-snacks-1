8400bc5eee8cca0d3ba6d3c6af0db4591c874902
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 12:29:55 +0200

    Fix formatting issues in main.js for improved readability

--- Snack/Snack4/main.js
+++ Snack/Snack4/main.js
@@ -1,11 +1,11 @@
 // TODO: Crea un array vuoto. Chiedi per 6 volte di inserire un numero.
 // TODO: Se è dispari inseriscilo nell'array.
 
-let numeri = []
+let numeri = [];
 for (let i = 0; i < 6; i++) {
   let numero = Number(prompt("Inserisci un numero"));
   if (numero % 2 === 1) {
-    numeri.push(numero)
+    numeri.push(numero);
   }
 }
-console.log(numeri)
+console.log(numeri);
