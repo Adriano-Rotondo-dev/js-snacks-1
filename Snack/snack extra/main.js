7622e83d1fa06e26e2755a9b308e78bc22bd6978
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 12:50:27 +0200

    Add initial HTML and JavaScript files for cube calculation functionality

---
+++ Snack/snack extra/main.js
@@ -0,0 +1,9 @@
+// TODO: Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
+
+let userN = Number(prompt("Inserisci il numero di cui vuoi ottenere il cubo"));
+
+for (let i = 0; i < userN; i++) {
+  let cube = i * i * i;
+  console.log("il cubo di " + userN + " è " + cube);
+  alert("il cubo di " + userN + " è " + cube);
+}
