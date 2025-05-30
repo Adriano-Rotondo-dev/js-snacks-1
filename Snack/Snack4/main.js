46e397d976f63b9d409e8b2aace3756e28a1340d
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 12:29:24 +0200

    Refactor number input logic to only store odd numbers in the array

--- Snack/Snack4/main.js
+++ Snack/Snack4/main.js
@@ -1,8 +1,11 @@
-// TODO: Crea un array vuoto. Chiedi per 6 volte di inserire un numero. Se è dispari inseriscilo nell'array.
+// TODO: Crea un array vuoto. Chiedi per 6 volte di inserire un numero.
+// TODO: Se è dispari inseriscilo nell'array.
 
 let numeri = []
 for (let i = 0; i < 6; i++) {
   let numero = Number(prompt("Inserisci un numero"));
-  numeri.push(numero)
+  if (numero % 2 === 1) {
+    numeri.push(numero)
+  }
 }
 console.log(numeri)
