439649488fa3311d5640388c7d9dacbed667f81f
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 13:25:05 +0200

    Fix loop condition to include 1000 in power of 2 calculations [NON ME RIESCE]

--- Snack/snack extra 2/main.js
+++ Snack/snack extra 2/main.js
@@ -1,7 +1,6 @@
 // TODO: stampa le potenze di 2 fino a 1000
 
 let potenza = [];
-for (let i = 2; i < 1000; i++) {
-    potenza.push(Math.pow(null))
-
+for (let i = 2; i <= 1000; i++) {
+  potenza.push(Math.pow(null));
 }
