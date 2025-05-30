d11581bbff3dd69d217404193a94e250e92f5163
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 11:34:22 +0200

    Implement functionality to compare two user-input words and display their lengths

--- Snack/Snack2/main.js
+++ Snack/Snack2/main.js
@@ -1,3 +1,23 @@
 // TODO: l'utente inserisce due parole in successione, con due prompt Il software stampa prima la parola più corta, poi la parola più lunga.
 
 
+ let earlyWord = prompt("Choose your first word")
+ console.log(earlyWord)
+ alert("Your first word is " + earlyWord)
+
+ let laterWord = prompt("Choose your second word")
+ console.log(laterWord)
+ alert("Your second word is " + laterWord)
+
+ if (earlyWord.length < laterWord.length) {
+    console.log(earlyWord + " is shorter than " + laterWord)
+    alert( earlyWord + " is shorter than " + laterWord)
+ }
+    else if (earlyWord.length > laterWord.length) {
+        console.log(laterWord  + " is shorter than " + earlyWord)
+        alert( laterWord  + " is shorter than " + earlyWord)
+    }
+    else if (earlyWord.length == laterWord.length) {
+        console.log ("Both words are the same length")
+        alert("Both words are the same length")
+    }
