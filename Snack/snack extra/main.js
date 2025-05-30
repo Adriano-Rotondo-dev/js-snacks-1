ed00cffdd3856d93fc55d3568d7b82dad78e6abe
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 12:58:03 +0200

    Fix loop iteration to correctly calculate cubes from 1 to N

--- Snack/snack extra/main.js
+++ Snack/snack extra/main.js
@@ -2,7 +2,7 @@
 
 let userN = Number(prompt("Inserisci il numero di cui vuoi ottenere il cubo"));
 
-for (let i = 0; i < userN; i++) {
+for (let i = 1; i <= userN; i++) {
   let cube = i * i * i;
   console.log("il cubo di " + i + " è " + cube);
 }
