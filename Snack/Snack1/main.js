d20d51e83e114e4b53e84b4fddc8c5fd1fc8607e
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 11:44:16 +0200

    Refactor JavaScript code for improved readability and consistency in number comparison

--- Snack/Snack1/main.js
+++ Snack/Snack1/main.js
@@ -9,18 +9,16 @@ console.log(biggerNumber);
 
 */
 
-
 // come doveva essere nel file perduto, non contarlo 😢
 
-let firstNumber = Number(prompt ("Enter your first choice of number"))
-console.log(firstNumber)
+let firstNumber = Number(prompt("Enter your first choice of number"));
+console.log(firstNumber);
 
-let secondNumber = Number(prompt ("Enter your second choice of number"))
-console.log(secondNumber)
+let secondNumber = Number(prompt("Enter your second choice of number"));
+console.log(secondNumber);
 
-alert("These are your chosen numbers " + firstNumber + " and " + secondNumber )
+alert("These are your chosen numbers " + firstNumber + " and " + secondNumber);
 
-let largeNumber = Math.max(firstNumber, secondNumber)
-console.log(largeNumber)
-alert("The largest number is " + largeNumber)
-
+let largeNumber = Math.max(firstNumber, secondNumber);
+console.log(largeNumber);
+alert("The largest number is " + largeNumber);
