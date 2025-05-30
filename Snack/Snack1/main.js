358267c864ea81f2f771fd96bcea7b7547712c49
Adriano-Rotondo-dev <adriano.rotondo.dev@gmail.com>
2025-05-30 11:01:21 +0200

    Add files via upload

---
+++ Snack/Snack1/main.js
@@ -0,0 +1,8 @@
+// TODO: l'utente stampa due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+const firstNumber = 10;
+const secondNumber = 15;
+console.log( firstNumber + " and " + secondNumber);
+
+const biggerNumber = Math.max(firstNumber, secondNumber)
+console.log(biggerNumber);
